325c3b3b7e6f26ac6a577897f623f68e0b1b167e
noodlexmurder <kc4338@nyu.edu>
2022-04-06 13:06:44 -0400

    updating rooms and room movement

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public Room roomToTheLeft;
+    public Room roomToTheRight;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/RoomMovement.cs
+++ Assets/Scripts/RoomMovement.cs
@@ -8,27 +8,35 @@ public class RoomMovement : MonoBehaviour
     public Vector3 rightRoomPosition;
     public Vector3 leftRoomPosition;
     public Vector3 backRoomPosition;
-
     public Vector3 startRoomPosition;
+
+    private Vector3 roomToTheRight;
+    private Vector3 roomToTheLeft;
+    private Vector3 currentRoom;
 
     public void Awake()
     {
-        startRoomPosition = Camera.main.transform.position;
+        currentRoom = startRoomPosition;
+    }
+
+    private void Update()
+    {
+        /*if (currentRoom == startRoomPosition)
+        {
+            roomToTheRight = rightRoomPosition;
+            roomToTheLeft = leftRoomPosition;
+        }*/
+
     }
 
     public void goRightRoom()
     {
-        Camera.main.transform.position = rightRoomPosition;
+        Camera.main.transform.position = roomToTheRight;
     }
 
     public void goLeftRoom()
     {
-        Camera.main.transform.position = leftRoomPosition;
-    }
-
-    public void goBackRoom()
-    {
-        Camera.main.transform.position = backRoomPosition;
+        Camera.main.transform.position = roomToTheLeft;
     }
 
 }
