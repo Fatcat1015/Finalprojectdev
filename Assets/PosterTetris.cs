924e8374beb212e2ba471c21012026778ba688dd
Trashbot <1732685232@qq.com>
2022-05-01 13:54:10 -0400

    Lord help me

--- Assets/PosterTetris.cs
+++ Assets/PosterTetris.cs
@@ -5,32 +5,102 @@ using UnityEngine;
 public class PosterTetris : MonoBehaviour
 {
 
-    private bool falling = true;
+    public bool falling = true;
+    public int color = 2;
+    public bool match_color_above;
+    public GameObject cubeabove;
+    public bool matched;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("cube" + color);
+    }
 
     void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        if (matched)
         {
-            if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x)
+            Destroy(gameObject);
+        }
+
+        if (falling)
+        {
+            if (Input.GetMouseButtonDown(0))
             {
-                transform.position += new Vector3(1, 0, 0);
+                if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x)
+                {
+                    transform.position += new Vector3(2, 0, 0);
                     //move right
-            }
-            else
-            {
-                transform.position += new Vector3(-1, 0, 0);
-                //move left
+                }
+                else
+                {
+                    transform.position += new Vector3(-2, 0, 0);
+                    //move left
+                }
             }
         }
 
+        GetComponent<SpriteRenderer>().sortingOrder = 110-Mathf.RoundToInt(transform.position.y*5);
+        //ytop 19
+        //y min12
         
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (!falling)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+            Debug.DrawRay(transform.position, -Vector3.up, Color.green);
+
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.GetComponent<PosterTetris>() != null)
+                {
+                    if (hit.collider.gameObject.GetComponent<PosterTetris>().color == this.color)
+                    {
+                        if (hit.collider.gameObject.GetComponent<PosterTetris>().falling == false)
+                        {
+                            match_color_above = true;
+                            cubeabove = hit.collider.gameObject;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        if (match_color_above)
+        {
+            /*
+            RaycastHit2D down = Physics2D.Raycast(transform.position, Vector2.down);
+
+            if (down.collider != null && down.collider.gameObject.GetComponent<PosterTetris>() != null)
+            {
+                if (down.collider.gameObject.GetComponent<PosterTetris>().color == this.color)
+                {
+                    if (down.collider.gameObject.GetComponent<PosterTetris>().match_color_above)
+                    {
+                        down.collider.gameObject.GetComponent<PosterTetris>().matched = true;
+                        FindObjectOfType<TetrisGM>().clear_count += 1;
+                        Destroy(down.collider.gameObject);
+                        Destroy(cubeabove);
+                        Destroy(gameObject);
+                    }
+                }
+            }*/
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
-        if (collision.gameObject.tag == "Tetris")
+        if (collision.gameObject.CompareTag("Tetris"))
         {
             falling = false;
             
--- Assets/TetrisGM.cs
+++ Assets/TetrisGM.cs
@@ -6,12 +6,60 @@ public class TetrisGM : MonoBehaviour
 {
     public bool playing;
     public GameObject cube;
+    public GameObject cube_prefab;
+    public Transform spawnpoint;
 
-    //generate random color cube
+    public List<GameObject> fallencubes = new List<GameObject>();
+
+    public int clear_count = 0;
 
     //detect and make cubes disappear
 
-    //detect if in the game or not
 
+    private void Update()
+    {
+        if (playing)    //detect if in the game or not
+        {
+            //start dispensing cubes
+            if(cube == null)
+            {
+                int cubecolor = Random.Range(1, 4);//randomnize color
+                cube = Instantiate(cube_prefab, spawnpoint.position, Quaternion.identity) as GameObject;
+                cube.GetComponent<PosterTetris>().color = cubecolor;
+            }
+            
+            if (cube.GetComponent<PosterTetris>().falling == false)
+            {
+                fallencubes.Add(cube);
+                cube = null;
+            }
+        }
+        else
+        {
+            for(int i = 0; i < fallencubes.Count; i++)
+            {
+                Destroy(fallencubes[i]);
+                fallencubes.Remove(fallencubes[i]);
+            }
+            Destroy(cube);
+            //reset status
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //playing = false;
+        }
+    }
 
 }
