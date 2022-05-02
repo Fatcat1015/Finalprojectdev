a786f6eeae896ffa4bd1394b0e1309cc141f10e7
kenneth <kc4338@nyu.edu>
2022-05-02 00:39:34 -0400

    imported cup assets
    and some scripts ?!! (barely)

---
+++ Assets/grandpa_cup_manager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grandpa_cup_manager : MonoBehaviour
+{
+    [Header("Cup Sprites")]
+    public SpriteRenderer cupWithGin;
+    public SpriteRenderer cupWithGinAndIce;
+    public SpriteRenderer cupWithPoison;
+    public SpriteRenderer cupWithPoisonAndIce;
+}
